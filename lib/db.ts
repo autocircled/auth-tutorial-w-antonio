f459db9d6bfdff8034dc885bdf36d3e442a28fa8
Moktadir Rahman <autocircled@gmail.com>
2024-12-05 09:38:26 +0600

    database & prisma setup done

---
+++ lib/db.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+    let prisma: PrismaClient | undefined
+}
+// export const db = globalThis.prisma || new PrismaClient();
+export const db = (globalThis as typeof globalThis & { prisma: PrismaClient | undefined }).prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") (globalThis as typeof globalThis & { prisma: PrismaClient | undefined }).prisma = db;
