01a8e1dffdc2186021160289e5a817338b898a71
Moktadir Rahman <autocircled@gmail.com>
2024-12-05 10:00:01 +0600

    create user done

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,10 @@
 "use server";
-
+import bcrypt from "bcrypt";
 import * as z from "zod";
 
 import { registerSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const Register = async (values: z.infer<typeof registerSchema>) => {
     const parsedValues = registerSchema.safeParse(values);
@@ -10,5 +12,22 @@ export const Register = async (values: z.infer<typeof registerSchema>) => {
         return { error: "Missing fields" };
     }
 
-    return { success: "Register successful" };
+    const { email, name, password } = parsedValues.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return { error: "Email already exists" };
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    });
+
+    return { success: "User created successfully" };
 }
